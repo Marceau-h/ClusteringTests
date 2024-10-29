fb5a4e63a8b081ae53d387b3e714310c2e695a67
Marceau-h <git@marceau-h.fr>
2024-10-29 16:20:58 +0100

    nice plots 👌

--- plot_gt_vs_clusters.py
+++ plot_gt_vs_clusters.py
@@ -3,6 +3,9 @@ from pathlib import Path
 import pandas as pd
 from tqdm.auto import tqdm
 import plotly.express as px
+from plotly.validators.scatter.marker import SymbolValidator
+
+raw_symbols = SymbolValidator().values
 
 # gt = "corpus_en/AGUILAR_home-influence/AGUILAR_home-influence_OCR/AGUILAR_Kraken/AGUILAR_home-influence_Kraken_GT_df_points.jsonl"
 #
@@ -16,21 +19,42 @@ df_gt = pd.read_json(gt, lines=True)
 df_hyp1 = pd.read_json(hyp1, lines=True)
 # df_hyp2 = pd.read_json(hyp2, lines=True)
 
-def plot_clusters(df_gt, df_hyp, names=('gt', 'hyp')):
+def from_float_to_pretty_string(x):
+    return f"cluster_{int(x):03d}" if x != "" else "Pas de cluster"
+
+
+def sort_the_pretty_strings(x):
+    return int(x.split("_")[1]) if x != "Pas de cluster" else -100
+
+
+def plot_clusters(
+        df_gt,
+        df_hyp,
+        livre="AIMARD_trappeurs",
+        ocr="Kraken",
+        gt="gt",
+        model="affprop_keep_vectors2"
+):
     df_all = pd.merge(
         df_gt,
         df_hyp,
         how='outer',
         on=['text'],
         suffixes=('_gt', '_hyp')
-    )
+    ).fillna("")
 
-    # df_all = df_all[["x_pos_hyp" is not None, "y_pos_hyp" is not None]]
     df_all = df_all[df_all.x_pos_hyp.notna()]
     df_all = df_all[df_all.y_pos_hyp.notna()]
 
-    print(df_all.columns)
-    print(df_all.head())
+    df_all["cluster_gt"] = df_all["cluster_gt"].apply(from_float_to_pretty_string)
+    df_all["cluster_hyp"] = df_all["cluster_hyp"].apply(from_float_to_pretty_string)
+
+    df_all = df_all.sort_values(by=["cluster_gt", "cluster_hyp"], key=lambda x: x.apply(sort_the_pretty_strings))
+
+    # df_all.to_csv(f"outp/{livre}_{ocr}_{gt}_{model}.csv", index=False)
+
+    # print(df_all.columns)
+    # print(df_all.head())
 
     fig = px.scatter(
         df_all,
@@ -38,19 +62,38 @@ def plot_clusters(df_gt, df_hyp, names=('gt', 'hyp')):
         y='y_pos_hyp',
         color='cluster_gt',
         symbol='cluster_hyp',
-        hover_data=['text']
+        hover_data=['text'],
+
     )
+    fig.update(layout_coloraxis_showscale=False, layout_showlegend=False)
     fig.update_traces(marker=dict(size=20))
     fig.update_layout(
-        title=f"Comparaison des clusters formés par {names[0]} et {names[1]}",
+        title=f"Comparaison des clusters formés par {gt} et {model} pour {livre} avec {ocr}",
         xaxis_title="",
         yaxis_title="",
     )
-    fig.show()
-    fig.write_html(f"outp/{names[0]}_vs_{names[1]}.html")
-    fig.write_image(f"outp/{names[0]}_vs_{names[1]}.png")
-    fig.write_image(f"outp/{names[0]}_vs_{names[1]}.eps")
+    # fig.update_layout(
+    #     legend={
+    #         "yanchor":"top",
+    #         "y":0.99,
+    #         "xanchor":"left",
+    #         "x":0.01,
+    #     }
+    # )
 
-plot_clusters(df_gt, df_hyp1, names=('gt', 'affprop_keep_vectors2'))
+    fig.show()
+    fig.write_html(f"outp/{livre}_{ocr}_{gt}_{model}.html")
+
+    fig.write_image(f"outp/{livre}_{ocr}_{gt}_{model}.png")
+    fig.write_image(f"outp/{livre}_{ocr}_{gt}_{model}.jpg")
+    fig.write_image(f"outp/{livre}_{ocr}_{gt}_{model}.jpeg")
+    fig.write_image(f"outp/{livre}_{ocr}_{gt}_{model}.webp")
+    fig.write_image(f"outp/{livre}_{ocr}_{gt}_{model}.svg")
+    fig.write_image(f"outp/{livre}_{ocr}_{gt}_{model}.pdf")
+    # fig.write_image(f"outp/{livre}_{ocr}_{gt}_{model}.eps")
+
+    fig.write_json(f"outp/{livre}_{ocr}_{gt}_{model}.json")
+
+plot_clusters(df_gt, df_hyp1, livre="AIMARD_trappeurs", ocr="Kraken", gt="gt", model="affprop_keep_vectors2")
 
 # plot_clusters(df_gt, df_hyp2, names=('gt', 'affprop_hyperparams2'))
